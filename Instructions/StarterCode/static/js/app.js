0ad0f25fbb1c377ff314b0d3b633d62e9f113c28
perkins228 <perkins.228@gmail.com>
2021-02-18 23:07:39 -0500

    save point

---
+++ Instructions/StarterCode/static/js/app.js
@@ -0,0 +1,35 @@
+function init(){
+  d3.json("static/js/samples.json").then(data => {
+    var id_names = data.names;
+    var dropdown = d3.select("#selDataset");
+    id_names.forEach(id => {
+        var option = dropdown.append("option");
+        option.text(id);
+        option.property("value", id);
+    });
+    var sample = id_names[0];
+    buildMetadata(sample);
+    // buildCharts(sample);
+});
+}
+
+function buildMetadata(sample){
+  d3.json("static/js/samples.json").then(data => {
+    var metadata = data.metadata;
+    var filterDemo = metadata.filter(i => i.id == sample);
+    console.log(filterDemo)
+    var table = d3.select("#sample-metadata")
+    Object.entries(filterDemo).forEach(([key,value]) => {
+      // table.append("div").text(`${key}: ${value}`)
+    })
+  })
+}
+// function buildCharts (sample) {
+//   return
+// }
+
+function optionChanged(option){
+
+}
+
+init()
